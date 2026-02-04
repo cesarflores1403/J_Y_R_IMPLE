c755cc707ff3042a3f1edf0d2851613746df18bf
ItzelleB24 <itzelle.banegas@unah.hn>
2026-02-04 11:45:47 -0600

    Delete backend directory

--- backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-// Configuración de la base de datos
-export const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'j_y_r_imple'
-};
